d5124f0a27eec0ea5356b1f63d3e1b0919bd731e
Rugile Vasaityte <rugile.vasaityte@gmail.com>
2024-04-11 15:17:51 +0300

    init

---
+++ src/main/java/BookExchange/BookCrosser/BookCrosserApplication.java
@@ -0,0 +1,13 @@
+package BookExchange.BookCrosser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrosserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCrosserApplication.class, args);
+	}
+
+}
